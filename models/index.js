e8b81337f95d63ee783a0d24263f037ba8fbddd5
alstonjohnson <johnson.alston@icloud.com>
2024-11-09 13:09:42 -0600

    updated index.js model

--- models/index.js
+++ models/index.js
@@ -9,19 +9,28 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
-
-Tag.belongsToMany(Product, {
-  foreignKey: 'product_id'
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
 });
 
-
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 module.exports = {
   Product,
